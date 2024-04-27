6f295d611225e7f97ddc6ff9a0e547f9eb5d67df
RonaldPOOP56 <ronaldopoop56@gmail.com>
2024-04-27 11:12:36 +0800

    Upload to Github

---
+++ airbnb/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import PropertyListing from './components/propertyListings'
+export default function Home() {
+return (
+<main>
+    <PropertyListing/>
+    
+</main>
+);
+}
